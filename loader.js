77fe272e85a3452e81cadd516e349ce265baccf1
0lder0 <lilii-ann.older@khk.ee>
2024-03-04 01:12:10 +0200

    loader small fix

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     const loader = document.querySelector('.loader');
 
     loader.classList.add('loader--hidden');
