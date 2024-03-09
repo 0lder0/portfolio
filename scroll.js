28c12ea74fc8c3d7895e59990b8906999d9b7d75
0lder0 <94623139+0lder0@users.noreply.github.com>
2024-03-09 21:41:29 +0200

    scroll icon made better (#2)

--- scroll.js
+++ scroll.js
@@ -1,10 +1,11 @@
 window.addEventListener('scroll', function() {
-    var arrow = document.getElementById('scrollArrow');
+    var arrow = document.getElementById('mouse-scroll');
+    var footer = document.querySelector('footer');
     var windowHeight = window.innerHeight;
     var bodyHeight = document.body.offsetHeight;
     var scrollTop = window.scrollY || window.pageYOffset;
 
-    if (scrollTop + windowHeight >= bodyHeight) {
+    if (scrollTop + windowHeight >= bodyHeight || scrollTop + windowHeight >= footer.offsetTop) {
         arrow.style.display = 'none';
     } else {
         arrow.style.display = 'block';
